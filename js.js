457feac3d2bcbe85515ef492483ccec261665281
devEstudante <alexsandromfaria@gmail.com>
2022-11-27 23:19:06 -0300

    versão inicial

---
+++ js.js
@@ -0,0 +1,15 @@
+var input = document.getElementById("input")
+var resultado = document.getElementById("res")
+var px = 16 
+
+function teste() {
+    var receiveInput = input.value
+
+    if(!receiveInput) {
+        window.alert('Impossível calcular, digite um núemro por favor.')
+    } else{
+        var converter = parseFloat(receiveInput)
+
+        resultado.innerHTML = (`O valor de ${converter} rem é equivalente á ${ converter * px}`)
+    } 
+}
